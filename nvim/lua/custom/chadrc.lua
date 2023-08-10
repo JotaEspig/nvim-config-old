b3bfa932d34ce1566a8609904b32002c7b835984
João Vitor Espig <jotinha1300@gmail.com>
2023-08-10 16:06:19 -0300

    background is transparent

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,5 +1,10 @@
 ---@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'oxocarbon'}
- M.plugins = 'custom.plugins'
- return M
+vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+
+local M = {}
+M.ui = {
+    theme = 'gruvbox'
+}
+M.plugins = 'custom.plugins'
+return M
